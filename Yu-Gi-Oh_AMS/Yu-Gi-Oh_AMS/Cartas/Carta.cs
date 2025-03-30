9b39381447ea8793e75100ce9dd8430c63193369
Miguel-Ar06 <marismendi.8551@unimar.edu.ve>
2025-03-30 02:57:50 -0400

    cositas

---
+++ Yu-Gi-Oh_AMS/Yu-Gi-Oh_AMS/Cartas/Carta.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio.CoreAudioApi;
+
+namespace Yu_Gi_Oh_AMS.Cartas
+{
+    internal class Carta
+    {
+        protected string nombre;
+        protected string descripcion;
+        protected string tipo;
+        protected Color color;
+        protected bool enMano;
+        protected bool enCampo;
+        protected bool enCementerio;
+        protected bool enMazo;
+        protected bool activada;
+        protected bool volteada;
+
+        protected string imagenReverso;
+        protected string imagen;
+    }
+
+    internal class Monstruo : Carta
+    {
+        public int ataque { get; set; }
+        public int defensa { get; set; }
+        // public int nivel { get; set; }
+        public string atributo { get; set; }
+        public string tipoMonstruo { get; set; }
+        public bool enModoAtaque { get; set; } // (1 = ataque, 0 = defensa)
+
+        Monstruo(int ataque, int defensa, string atributo, string tipo_monstruo, string nombre, string descripcion, string tipo, Color color, string imagen)
+        {
+            this.ataque = ataque;
+            this.defensa = defensa;
+            this.atributo = atributo;
+            this.tipoMonstruo = tipo_monstruo;
+            this.nombre = nombre;
+            this.descripcion = descripcion;
+            this.tipo = tipo;
+            this.color = color;
+            volteada = false;
+            enModoAtaque = true;
+            enCampo = false;
+            enCementerio = false;
+            enMano = false;
+            enMazo = false;
+            activada = false;
+            this.imagen = imagen;
+        }
+
+        public int atacar (Monstruo objetivo)
+        {
+            int diferencia;
+
+            if (objetivo.enModoAtaque)
+            {
+                diferencia = ataque - objetivo.ataque;
+
+                if (diferencia > 0)
+                {
+                    objetivo.enCementerio = true;
+                    return diferencia;
+                }
+                else if (diferencia < 0)
+                {
+                    enCementerio = true;
+                    return diferencia;
+                }
+                else
+                {
+                    enCementerio = true;
+                    objetivo.enCementerio = true;
+                    return 0;
+                }
+            }
+            else
+            {
+                diferencia = ataque - objetivo.defensa;
+
+                if (diferencia > 0)
+                {
+                    objetivo.enCementerio = true;
+                    return diferencia;
+                }
+                else if (diferencia < 0)
+                {
+                    return diferencia;
+                }
+                else
+                {
+                    objetivo.enCementerio = true;
+                    return 0;
+                }
+            }
+        }
+    }
+
+    internal class Hechizo : Carta
+    {
+        public string efecto { get; set; }
+        // int duracion;
+
+        public Hechizo (string efecto, string nombre, string descripcion, string tipo, Color color, string imagen)
+        {
+            this.efecto = efecto;
+            this.nombre = nombre;
+            this.descripcion = descripcion;
+            this.tipo = tipo;
+            this.color = color;
+            volteada = false;
+            enCampo = false;
+            enCementerio = false;
+            enMano = false;
+            enMazo = false;
+            activada = false;
+            this.imagen = imagen;
+        }
+    }
+
+    internal class Trampa : Carta
+    {
+        public string efecto { get; set; }
+        // int duracion;
+        public Trampa(string efecto, string nombre, string descripcion, string tipo, Color color, string imagen)
+        {
+            this.efecto = efecto;
+            this.nombre = nombre;
+            this.descripcion = descripcion;
+            this.tipo = tipo;
+            this.color = color;
+            volteada = false;
+            enCampo = false;
+            enCementerio = false;
+            enMano = false;
+            enMazo = false;
+            activada = false;
+            this.imagen = imagen;
+        }
+    }
+}
---
+++ Yu-Gi-Oh_AMS/Yu-Gi-Oh_AMS/Estructuras.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yu_Gi_Oh_AMS
+{
+    #region Lista Simple y Nodo
+
+    internal unsafe class Nodo<T> where T : class
+    {
+        public T dato { get; set; }
+        public Nodo<T>* siguiente { get; set; }
+        public int indice { get; set; }
+    }
+
+    internal unsafe class ListaSimple<T> where T : class
+    {
+        public Nodo<T>* cabeza { get; set; }
+        public Nodo<T>* cola { get; set; }
+
+        int tamano { get; set; }
+
+        public ListaSimple()
+        {
+            cabeza = null;
+            cola = null;
+        }
+
+        public void agregar(T dato)
+        {
+            Nodo<T>* nuevoNodo = (Nodo<T>*)Marshal.AllocHGlobal(sizeof(Nodo<T>));
+            nuevoNodo->dato = dato;
+            nuevoNodo->siguiente = null;
+
+            if (cabeza == null)
+            {
+                cabeza = nuevoNodo;
+                cola = nuevoNodo;
+            }
+            else
+            {
+                cola->siguiente = nuevoNodo;
+                cola = nuevoNodo;
+            }
+
+            tamano++;
+            indexar();
+        }
+
+        public void eliminarPorDato(T dato)
+        {
+            Nodo<T>* actual = cabeza;
+            Nodo<T>* anterior = null;
+
+            while (actual != null)
+            {
+                if (actual->dato == dato)
+                {
+                    if (anterior == null)
+                    {
+                        cabeza = actual->siguiente;
+                    }
+                    else
+                    {
+                        anterior->siguiente = actual->siguiente;
+                    }
+
+                    tamano--;
+                    Marshal.FreeHGlobal((IntPtr)actual);
+                    break;
+                }
+
+                anterior = actual;
+                actual = actual->siguiente;
+            }
+
+            indexar();
+        }
+
+        public void eliminarPorIndice(int indice)
+        {
+            Nodo<T>* actual = cabeza;
+            Nodo<T>* anterior = null;
+
+            while (actual != null)
+            {
+                if (actual->indice == indice)
+                {
+                    if (anterior == null)
+                    {
+                        cabeza = actual->siguiente;
+                    }
+                    else
+                    {
+                        anterior->siguiente = actual->siguiente;
+                    }
+
+                    tamano--;
+                    Marshal.FreeHGlobal((IntPtr)actual);
+                    break;
+                }
+                anterior = actual;
+                actual = actual->siguiente;
+            }
+
+            indexar();
+        }
+
+        public void indexar()
+        {
+            Nodo<T>* actual = cabeza;
+            int indice = 0;
+
+            while (actual != null)
+            {
+                actual->indice = indice;
+                indice++;
+                actual = actual->siguiente;
+            }
+        }
+
+        public T obtenerPorIndice(int indice)
+        {
+            Nodo<T>* actual = cabeza;
+
+            while (actual != null)
+            {
+                if (actual->indice == indice)
+                {
+                    return actual->dato;
+                }
+                actual = actual->siguiente;
+            }
+            return null;
+        }
+
+        public int obtenerIndicePorDato(T dato)
+        {
+            Nodo<T>* actual = cabeza;
+
+            while (actual != null)
+            {
+                if (actual->dato == dato)
+                {
+                    return actual->indice;
+                }
+                actual = actual->siguiente;
+            }
+            return -1;
+        }
+    }
+
+    #endregion
+}
